bde5009699125575b092f76d41cce5532a384c61
molko <molkoback@gmail.com>
2020-01-27 19:15:28 +0200

    HW2: implement activities to link interfaces layouts to actions

--- app/src/main/java/com/molko/assistedreminder/MainActivity.kt
+++ app/src/main/java/com/molko/assistedreminder/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.molko.assistedreminder
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val fabTime: View = findViewById(R.id.fabTime)
+        fabTime.setOnClickListener { view ->
+            val intent = Intent(this, TimeActivity::class.java)
+            startActivity(intent)
+        }
+        val fabMap: View = findViewById(R.id.fabMap)
+        fabMap.setOnClickListener { view ->
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
+        val fabHelp: View = findViewById(R.id.fabHelp)
+        fabHelp.setOnClickListener { view ->
+        }
     }
 }
