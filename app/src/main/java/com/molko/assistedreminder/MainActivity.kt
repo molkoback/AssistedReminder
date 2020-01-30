e1ce3e41c232d35c9751b365fb24be40a3515410
molko <molkoback@gmail.com>
2020-01-30 20:53:19 +0200

    HW2: improved layout

--- app/src/main/java/com/molko/assistedreminder/MainActivity.kt
+++ app/src/main/java/com/molko/assistedreminder/MainActivity.kt
@@ -3,25 +3,34 @@ package com.molko.assistedreminder
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
-        val fabTime: View = findViewById(R.id.fabTime)
-        fabTime.setOnClickListener { view ->
-            val intent = Intent(this, TimeActivity::class.java)
-            startActivity(intent)
+        var addActive = false
+        fabAdd.setOnClickListener {
+            if (!addActive) {
+                addActive = true
+                fabMap.animate().translationY(-1*resources.getDimension(R.dimen.add_animation))
+                fabTime.animate().translationY(-2*resources.getDimension(R.dimen.add_animation))
+            }
+            else {
+                addActive = false
+                fabMap.animate().translationY(0f)
+                fabTime.animate().translationY(0f)
+            }
         }
-        val fabMap: View = findViewById(R.id.fabMap)
-        fabMap.setOnClickListener { view ->
+        
+        fabMap.setOnClickListener {
             val intent = Intent(this, MapActivity::class.java)
             startActivity(intent)
         }
-        val fabHelp: View = findViewById(R.id.fabHelp)
-        fabHelp.setOnClickListener { view ->
+        fabTime.setOnClickListener {
+            val intent = Intent(this, TimeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
